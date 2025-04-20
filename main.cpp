1104836d594f90b12fb1cd78afc6aa22f19d70e2
adama-wzr <adama@ku.edu>
2025-04-19 21:21:50 -0400

    Started Project
    - I will use this project to test a bunch of things related to cmake files and C/C++ project building.
    - I prefer C coding, so this will be C++ but written like C

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+#include <stdlib.h>
+
+
+int main (int argc, char *argv[]) {
+  
+  printf("I am trying to get used to this.\n");
+  
+  printf("HelloWolrd!\n");
+    
+  return 0;
+}
